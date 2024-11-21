b29350b4d6985372727f48d2faf3986baa9ef1f7
willamhs <willamhuamansaavedra13@gmail.com>
2024-11-21 05:01:52 -0500

    Cambio de url de paypal en el archivo de envionmet

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,6 @@ export const environment = {
     production: true,
     //baseURL: 'http://localhost:8080/api/v1',
     baseURL: 'https://holdme-api-latest.onrender.com/api/v1',
-    paypalReturnUrl: 'http://localhost:4200/customer/cart'
+    //paypalReturnUrl: 'http://localhost:4200/customer/cart'
+    paypalReturnUrl: 'https://holdme-web-v1.netlify.app/customer/cart'
 };
